7bbb136cd0e0b47ef55ff9f445910b2a3011f440
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-07-15 22:11:47 +0200

    Navigation From Activity

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit  var   binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
