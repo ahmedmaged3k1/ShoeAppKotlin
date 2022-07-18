073352e208316d6491862d5b9a4e5b4618236602
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-07-18 17:51:38 +0200

    Add Shoe

---
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -0,0 +1,31 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+
+
+
+class ShoeDetailFragment : Fragment() {
+
+
+
+    private lateinit var binding: FragmentShoeDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        // Inflate the layout for this fragment
+        binding = FragmentShoeDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+
+
+}
--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
 
@@ -18,8 +19,19 @@ class ShoeListFragment : Fragment() {
 
         // Inflate the layout for this fragment
         binding = FragmentShoeListBinding.inflate(inflater, container, false)
+        buttonOnClick()
         return binding.root
     }
+    private fun buttonOnClick() {
+        binding.addShoe.setOnClickListener {
+            navigate(it)
+        }
 
+    }
+
+    private fun navigate(view: View) {
+
+        view.findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
+    }
 
 }
