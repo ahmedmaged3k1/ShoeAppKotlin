94f94afde40f39ca71016673d0cbdcb5197e79e4
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-07-19 19:04:57 +0200

    2 way DataBinding

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -1,20 +1,23 @@
 package com.udacity.shoestore
 
 
+
 import android.os.Bundle
+
 
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-import android.widget.Toast
+
+
 import androidx.fragment.app.activityViewModels
 
 import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
-import com.udacity.shoestore.models.Shoe
+
 
 
 class ShoeDetailFragment : Fragment() {
@@ -23,6 +26,7 @@ class ShoeDetailFragment : Fragment() {
 
     private lateinit var binding: FragmentShoeDetailBinding
     private  val viewModel: ShoesViewModel by activityViewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +39,8 @@ class ShoeDetailFragment : Fragment() {
         binding = FragmentShoeDetailBinding.inflate(inflater, container, false)
         buttonCancelOnClick()
         buttonSaveOnClick()
-
+        binding.shoeList=viewModel
+        binding.lifecycleOwner =this
         return binding.root
     }
 
@@ -48,16 +53,8 @@ class ShoeDetailFragment : Fragment() {
     }
     private fun buttonSaveOnClick() {
         binding.appCompatButton.setOnClickListener {
-            if (shoeValidation())
-            {
-                val shoe = Shoe(binding.editText3.text.toString(),binding.editText5.text.toString().toDouble(),
-                    binding.editText4.text.toString(),binding.editText6.text.toString())
-                viewModel.addShoe(shoe)
-
+                viewModel.addShoe()
                 navigate(it)
-            }
-
-
         }
 
     }
@@ -67,20 +64,6 @@ class ShoeDetailFragment : Fragment() {
         view.findNavController().navigate(R.id.action_shoeDetailFragment_to_shoeListFragment)
     }
 
-    private  fun shoeValidation() : Boolean{
-        if (binding.editText3.text.isEmpty()||binding.editText4.text.isEmpty()
-            ||binding.editText5.text.isEmpty()||binding.editText6.text.isEmpty())
-        {
-            val text = "Please Enter All Fields"
-            val duration = Toast.LENGTH_SHORT
-            val applicationContext = activity
-            Toast.makeText(applicationContext, text, duration).show()
-
-            return false
-
-        }
-        return true
-    }
 
 
 }
--- app/src/main/java/com/udacity/shoestore/ShoesViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoesViewModel.kt
@@ -7,12 +7,24 @@ import com.udacity.shoestore.models.Shoe
 
 class ShoesViewModel: ViewModel() {
     val shoesList  = MutableLiveData<ArrayList<Shoe>>()
+    var shoeName = MutableLiveData<String>()
+    var shoeDescription = MutableLiveData<String>()
+    var shoeCompany = MutableLiveData<String>()
+    var shoeSize = MutableLiveData<String>()
     private var shoesArrayList = ArrayList<Shoe>()
 
-    fun addShoe(shoe: Shoe)
+
+    fun addShoe()
     {
+         val shoe = Shoe(shoeName.value.toString(),shoeSize.value.toString().toDouble(),shoeCompany
+             .value.toString(),shoeDescription.value.toString())
         shoesArrayList.add(shoe)
         shoesList.value=shoesArrayList
+        shoeName.value=""
+        shoeSize.value=""
+        shoeDescription.value=""
+        shoeCompany.value=""
+
     }
 
 
