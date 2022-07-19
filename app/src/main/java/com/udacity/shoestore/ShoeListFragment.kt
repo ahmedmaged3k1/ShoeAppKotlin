56c1f281443b27a9304815f413faca9c83c19662
ahmedmaged3k1 <60134186+ahmedmaged3k1@users.noreply.github.com>
2022-07-19 21:08:13 +0200

    Fixed Logout

--- app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -1,17 +1,19 @@
 package com.udacity.shoestore
 
 
+
 import android.os.Bundle
+import android.view.*
 
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 
+
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
 
@@ -29,8 +31,21 @@ class ShoeListFragment : Fragment() {
         buttonOnClick()
         createViews()
        // Log.d(TAG, "Testing : size  "+ (viewModel.shoesList.value?.get(0)?.company ?: 258))
+        setHasOptionsMenu(true)
             return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu,menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        NavigationUI.onNavDestinationSelected(item,view!!.findNavController())
+        return super.onOptionsItemSelected(item)
+    }
+
+
     private fun createViews(){
 
         viewModel.shoesList.observe(viewLifecycleOwner, Observer {
